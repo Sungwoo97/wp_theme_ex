9ac2d4be794df11b9d1573b8f6ce1a816528cc42
Sungwoo97 <gwaja97@gmail.com>
2024-12-02 15:00:52 +0900

    워드프레스로 포트폴리오 제작하기 1일차

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+register_nav_menus(array(
+  'main_menu'   => __('Main Menu', 'ssungwoo_portfolio'),
+  'footer_menu' => __('Footer Menu', 'ssungwoo_portfolio')
+));
+
+add_theme_support('post-thumbnails');
--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<h1>MY Portfolio</h1>
+<?php get_header() ?>
+
+<div id="content" class="two-thirds">
+  <?php
+  if (have_posts()) :
+    while (have_posts()) : the_post();
+      the_title('<h2>', '</h2>');
+      the_content();
+    endwhile;
+  else :
+    _e('Sorry, no posts matched your criteria.', 'ssungwoo_portfolio');
+  endif;
+  ?>
+</div><!-- #content-->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
